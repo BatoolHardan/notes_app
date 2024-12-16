f101670c612ad39f9329c925efd3b1a91d31484f
Batool Hardan <batool.hardan2001@gmail.com>
2024-12-16 11:41:55 +0200

    add widgets

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app/views/widgets/notes_view_body.dart';
 
 class NotesApp extends StatelessWidget {
   const NotesApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return const Scaffold(
+      body: NotesViewBody(),
+    );
   }
 }
