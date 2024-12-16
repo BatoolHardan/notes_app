2d09f2226e3ccb792f28d498d70c8560bd94ba73
Batool Hardan <batool.hardan2001@gmail.com>
2024-12-16 14:03:18 +0200

    create custom note item

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:notes_app/views/widgets/notes_view_body.dart';
 
-class NotesApp extends StatelessWidget {
-  const NotesApp({super.key});
+class NotesView extends StatelessWidget {
+  const NotesView({super.key});
 
   @override
   Widget build(BuildContext context) {
